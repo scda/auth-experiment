f73bb14ff4ef379a15a840be53de9bd71cedd565
scda <dsop@mailbox.org>
2021-05-01 15:12:47 +0200

    add basic prometheus metrics integration

--- Startup.cs
+++ Startup.cs
@@ -1,24 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using authservice.Data;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using authservice.Data;
+using Prometheus;
 
 namespace authservice
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration) { Configuration = configuration; }
 
         public IConfiguration Configuration { get; }
 
@@ -29,15 +21,15 @@ namespace authservice
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddHealthChecks()
+                    .ForwardToPrometheus();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
             else
             {
                 app.UseExceptionHandler("/Error");
@@ -49,11 +41,17 @@ namespace authservice
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseHttpMetrics(options =>
+            {
+                // This identifies the page when using Razor Pages.
+                options.AddRouteParameter("page");
+            });
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
+                endpoints.MapMetrics();
             });
         }
     }
